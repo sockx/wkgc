d7aafb4c835f179aaf5f8114965a36ec2f694548
3beltone <32082618+3beltone@users.noreply.github.com>
2021-07-22 16:48:43 +0800

    同步更新入口操作

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"wkgc/lib/core/config"
+	"wkgc/lib/utils/iot"
 )
 
 func main() {
 	// load config
 	config.Load()
+	// checkDataDir
+	iotool := iot.NewIoTool()
+	iotool.CheckAndCreateDir(config.Config.WorkDir)
 	// todo
+
 }
