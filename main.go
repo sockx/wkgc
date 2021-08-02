d5819ed6de0705b59f73f106da835379af7e370b
3beltone <32082618+3beltone@users.noreply.github.com>
2021-08-02 16:26:48 +0800

    update main.go

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"wkgc/lib/core/config"
+	"wkgc/lib/utils/dbt"
 	"wkgc/lib/utils/iot"
 )
 
@@ -11,6 +12,8 @@ func main() {
 	// checkDataDir
 	iotool := iot.NewIoTool()
 	iotool.CheckAndCreateDir(config.Config.WorkDir)
+	// Init LocalDatabase - Create Table
+	dbt.InitLocalDatabase()
 	// todo
 
 }
