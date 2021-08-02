fb69a2e830b23896816d2ce248a74a140bb2e9f4
3beltone <32082618+3beltone@users.noreply.github.com>
2021-08-02 16:26:23 +0800

    add sqlite3 database util

---
+++ lib/utils/dbt/dbt.go
@@ -0,0 +1,120 @@
+package dbt
+
+import (
+	"database/sql"
+	"wkgc/lib/core/config"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+/*
+	Dirinfo Model
+*/
+type DirInfoClass struct {
+	Did      int    `col:"did" json:"did"`
+	Dirname  string `col:"dirname" json:"dirname"`
+	Describe string `col:"describe" json:"describe"`
+	Isgit    bool   `col:"isgit" json:"isgit"`
+	Created  string `col:"created" json:"created"`
+}
+
+/*
+	Select Info to Model object
+*/
+func (dic *DirInfoClass) ParseResult(rows *sql.Rows) {
+	for rows.Next() {
+		rows.Scan(&dic.Did, &dic.Dirname, &dic.Describe, &dic.Isgit, &dic.Created)
+	}
+}
+
+/*
+	Init LocalDatabase
+	Create Table if Table not exists
+*/
+func InitLocalDatabase() {
+	db := OpenLocalDatabase()
+	sql_table := `
+	CREATE TABLE IF NOT EXISTS "dirinfo" (
+		"did" INTEGER PRIMARY KEY AUTOINCREMENT,
+		"dirname" VARCHAR(64) NULL,
+		"describe" VARCHAR(255) NULL,
+		"isgit" Bool NULL,
+		"created" TIMESTAMP default (datetime('now', 'localtime'))  
+	);`
+	db.Exec(sql_table)
+	db.Close()
+}
+
+/*
+	Return New sqlite3 sql.DB object
+*/
+func OpenLocalDatabase() *sql.DB {
+	db, err := sql.Open("sqlite3", config.Config.Database)
+	checkErr(err)
+	return db
+}
+
+/*
+	Add DirInfo to database
+*/
+func AddDirInfo(dirname string, describe string, isgit bool) {
+	db := OpenLocalDatabase()
+	stmt, err := db.Prepare("INSERT INTO dirinfo(dirname, describe, isgit) values(?,?,?)")
+	checkErr(err)
+	stmt.Exec(dirname, describe, isgit)
+	db.Close()
+}
+
+/*
+	Select DirInfo from dataase
+*/
+func SelectDirinfoByDid(did int) *DirInfoClass {
+	db := OpenLocalDatabase()
+	stmt, err := db.Prepare("SELECT * FROM dirinfo WHERE did=?")
+	checkErr(err)
+	thisrows, err := stmt.Query(did)
+	checkErr(err)
+	// 创建对象
+	dinfo := new(DirInfoClass)
+	// 解析对象
+	dinfo.ParseResult(thisrows)
+	db.Close()
+	return dinfo
+}
+
+/*
+	Update DirInfo in the database
+*/
+func UpdateDirInfo(dirinfobj DirInfoClass) {
+	db := OpenLocalDatabase()
+	stmt, err := db.Prepare("update dirinfo set dirname=?,describe=?,isgit=?,created=?  where did=?")
+	checkErr(err)
+	stmt.Exec(dirinfobj.Dirname, dirinfobj.Describe, dirinfobj.Isgit, dirinfobj.Created, dirinfobj.Did)
+	db.Close()
+}
+
+/*
+	Delete DirInfo by did
+*/
+func DeleteDirInfoByDid(did int) bool {
+	db := OpenLocalDatabase()
+	stmt, err := db.Prepare("DELETE FROM dirinfo WHERE did=?")
+	checkErr(err)
+	res, err := stmt.Exec(did)
+	checkErr(err)
+	affect, err := res.RowsAffected()
+	ret := false
+	if affect > 0 {
+		ret = true
+	}
+	return ret
+}
+
+/*
+	Check Error
+*/
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
